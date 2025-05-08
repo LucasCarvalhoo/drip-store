4362517f5ab75e770bd407f16143acc696a95c54
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2025-05-08 09:11:39 -0300

    fix: ajuste de imports e para visualizar o component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
+// src/App.jsx
+import React from 'react'
 import './App.css'
+import ProdutosEmAlta from './components/Product-card/ProdutosEmAlta'
 
 function App() {
-
   return (
-    <BrowserRouter>
-    <ToastContainer/>
-      <AppRoutes/>
-    </BrowserRouter>
+    <div className="container mx-auto px-4">
+      <ProdutosEmAlta />
+    </div>
   )
 }
 
